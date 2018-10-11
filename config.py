609545f927f2a60daf2b09fbb9688e1a35889d22
notZombieFood <gerardocd1000@gmail.com>
2018-10-11 18:39:03 -0500

    fixes for heroku

--- config.py
+++ config.py
@@ -6,4 +6,5 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'R4t4t4'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
